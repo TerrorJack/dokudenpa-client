2368d195b4761185a116320f5d588fc921b7d6cc
Shao Cheng <astrohavoc@gmail.com>
2018-01-03 00:03:33 +0800

    RTWIP

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-import Terminal from "xterm";
-import * as Chalk from "chalk";
-
-function sleep(interval) {
-    return new Promise(resolve => setTimeout(() => resolve(null), interval));
-}
-
 (async () => {
-    const term = new Terminal({cursorBlink: true, rows: 64});
+    const Terminal = require("xterm");
+    const Chalk = require("chalk");
+
+    const sleep = interval => new Promise(resolve => setTimeout(() => resolve(null), interval));
+    const fire = src => new Promise(resolve => {
+        const el = document.createElement("script");
+        el.res = resolve;
+        el.innerText = src + "\n;document.currentScript.res(null);delete document.currentScript.res;";
+        document.body.appendChild(el);
+    });
+
+    window.term = new Terminal({cursorBlink: true, rows: 64});
     term.writeWithInterval = interval => async (text) => {
         for (let i = 0; i < text.length; ++i) {
             term.write(text[i]);
             await sleep(interval);
         }
     };
-    const chalk = new Chalk.constructor({enabled: true, level: 2});
-
     term.open(document.body, true);
     term.element.classList["add"]("fullscreen");
+
+    const chalk = new Chalk.constructor({enabled: true, level: 2});
 
     const f = term.writeWithInterval(128);
     await f(chalk.green("HELLO"));
     await f(" WORLD");
+    console.log(await fire("term.writeWithInterval(128)('\\nAND YOU.');"));
 })();
